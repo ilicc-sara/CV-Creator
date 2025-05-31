e01b146b461ab88841bdcbc8ab5bde0a6696e5be
Sara <sarailic431@gmail.com>
2025-05-31 18:17:28 +0200

    starting app, styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,22 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="inputs-container">
+        <div className="personal-info-cont">
+          <h3>Personal Information</h3>
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+          <input type="text" placeholder="Title" />
+          <input type="text" placeholder="Address" />
+          <input type="tel" placeholder="Phone number" />
+          <input type="email" placeholder="Email" />
+          <input type="text" placeholder="Description" />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
