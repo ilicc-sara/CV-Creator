e6c34293008ad27e90200ff292a26a77ee376a2e
Sara <sarailic431@gmail.com>
2025-06-04 19:44:29 +0200

    seting cv preview

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import EducationInfo from "./EducationInfo";
 import ExperienceInfo from "./ExperienceInfo";
 
 function App() {
+  const [previewCV, setPreviewCV] = useState(false);
   const [personalInfo, setPersonalInfo] = useState({
     firstName: "",
     lastName: "",
@@ -43,6 +44,8 @@ function App() {
     e.preventDefault();
 
     console.log(personalInfo, educationInfo, experienceInfo);
+
+    setPreviewCV(true);
   }
 
   function reset() {
@@ -84,80 +87,134 @@ function App() {
   }
   return (
     <>
-      <form onSubmit={handleSubmit} className="inputs-container">
-        <PersonalInfo
-          key={1}
-          setPersonalInfo={setPersonalInfo}
-          id={personalInfo.id}
-        />
-
-        {educationInfo.map((educationInfo, index) => (
-          <EducationInfo
-            key={index}
-            id={educationInfo.id}
-            setEducationInfo={setEducationInfo}
+      {!previewCV && (
+        <form onSubmit={handleSubmit} className="inputs-container">
+          <PersonalInfo
+            key={1}
+            setPersonalInfo={setPersonalInfo}
+            id={personalInfo.id}
           />
-        ))}
-        <button
-          type="button"
-          className="add-education"
-          onClick={() =>
-            setEducationInfo((prev) => [
-              ...prev,
-              {
-                uniName: "",
-                city: "",
-                degree: "",
-                subject: "",
-                startDate: "",
-                endDate: "",
-                id: crypto.randomUUID(),
-              },
-            ])
-          }
-        >
-          Add Education
-        </button>
 
-        {experienceInfo.map((experienceInfo, index) => (
-          <ExperienceInfo
-            key={index}
-            id={experienceInfo.id}
-            setExperienceInfo={setExperienceInfo}
-          />
-        ))}
+          {educationInfo.map((educationInfo, index) => (
+            <EducationInfo
+              key={index}
+              id={educationInfo.id}
+              setEducationInfo={setEducationInfo}
+            />
+          ))}
+          <button
+            type="button"
+            className="add-education"
+            onClick={() =>
+              setEducationInfo((prev) => [
+                ...prev,
+                {
+                  uniName: "",
+                  city: "",
+                  degree: "",
+                  subject: "",
+                  startDate: "",
+                  endDate: "",
+                  id: crypto.randomUUID(),
+                },
+              ])
+            }
+          >
+            Add Education
+          </button>
 
-        <button
-          type="button"
-          className="add-experience"
-          onClick={() =>
-            setExperienceInfo((prev) => [
-              ...prev,
-              {
-                position: "",
-                company: "",
-                city: "",
-                startDate: "",
-                endDate: "",
-                id: crypto.randomUUID(),
-              },
-            ])
-          }
-        >
-          Add Experience
-        </button>
+          {experienceInfo.map((experienceInfo, index) => (
+            <ExperienceInfo
+              key={index}
+              id={experienceInfo.id}
+              setExperienceInfo={setExperienceInfo}
+            />
+          ))}
 
-        <button className="finish-btn btn-preview" type="submit">
-          Preview
-        </button>
-        <button
-          className="finish-btn btn-reset"
-          type="button"
-          onClick={() => reset()}
-        >
-          Reset
-        </button>
-      </form>
+          <button
+            type="button"
+            className="add-experience"
+            onClick={() =>
+              setExperienceInfo((prev) => [
+                ...prev,
+                {
+                  position: "",
+                  company: "",
+                  city: "",
+                  startDate: "",
+                  endDate: "",
+                  id: crypto.randomUUID(),
+                },
+              ])
+            }
+          >
+            Add Experience
+          </button>
+
+          <button className="finish-btn btn-preview" type="submit">
+            Preview
+          </button>
+          <button
+            className="finish-btn btn-reset"
+            type="button"
+            onClick={() => reset()}
+          >
+            Reset
+          </button>
+        </form>
+      )}
+
+      {previewCV && (
+        <div className="preview-page">
+          <nav className="nav">
+            <h2> {personalInfo.firstName} </h2>
+            <h4> {personalInfo.title} </h4>
+          </nav>
+
+          <main className="main">
+            <div className="details-cont">
+              <div>
+                <h3>Description</h3>
+                <p> {personalInfo.description} </p>
+              </div>
+
+              <div>
+                <h3>Education</h3>
+                <p> {educationInfo[0].uniName} </p>
+              </div>
+
+              <div>
+                <h3>Experience</h3>
+                <p> {experienceInfo[0].position} </p>
+              </div>
+            </div>
+
+            <div className="personal-details-cont">
+              <span>
+                {" "}
+                <img scr="./profilePhoto.jpeg" />
+              </span>
+
+              <h3>Personal Details</h3>
+
+              <div>
+                <h4> Addrress </h4>
+                <p> {personalInfo.address} </p>
+              </div>
+
+              <div>
+                <h4> Phone Number </h4>
+                <p> {personalInfo.phoneNumber} </p>
+              </div>
+
+              <div>
+                <h4> Email </h4>
+                <p> {personalInfo.email} </p>
+              </div>
+            </div>
+          </main>
+        </div>
+      )}
     </>
   );
 }
